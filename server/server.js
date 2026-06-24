8144ae436bb8049911c54a88ddbe286c581255b4
M Prashant Rao <mprashantrao9@gmail.com>
2026-06-24 17:38:45 +0530

    Fix CORS for Vercel

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB()
 
 // Middlewares
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174',  'https://blog-ai-1.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Expires', 'If-None-Match'],
